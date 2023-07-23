7f1d8562fd5ccdc4b52d88c0f44a920b9d53a325
Meacha1 <meachattd@gmail.com>
2023-07-23 14:05:14 +0300

    2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,20 +6,18 @@ import math
 
 
 def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-    """Return a dictionary for the pagination"""
+    '''Return a dictionary with pagination info'''
+    assert type(page) == int and page > 0
+    assert type(page_size) == int and page_size > 0
 
     data = self.get_page(page, page_size)
-
     total_pages = math.ceil(len(self.dataset()) / page_size)
 
-    next_page = page + 1 if page < total_pages else None
-    prev_page = page - 1 if page > 1 else None
-
     return {
-      'page_size': len(data),
-      'page': page,
-      'data': data,
-      'next_page': next_page,
-      'prev_page': prev_page,
-      'total_pages': total_pages
+        'page_size': len(data),
+        'page': page,
+        'data': data,
+        'next_page': page + 1 if page + 1 < total_pages else None,
+        'prev_page': page - 1 if page > 1 else None,
+        'total_pages': total_pages
     }
