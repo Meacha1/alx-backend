78245713714c607c94091b6af204a0cb1f921fa6
Meacha1 <meachattd@gmail.com>
2023-08-01 22:00:50 +0300

    pycode

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,7 +25,7 @@ def get_locale():
     for query in queries:
         if 'locale=' in query:
             return query.split('=')[1]
-    
+
     # if locale not in app.config['LANGUAGES']:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
