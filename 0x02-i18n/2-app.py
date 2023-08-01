28ba96e824fc420855a72c8f9e808fd62b869b64
Meacha1 <meachattd@gmail.com>
2023-08-01 21:52:18 +0300

    get_local

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''Basic flask app, with a single route and template'''
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config(object):
+    '''Config class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''Get locale'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world():
+    '''Hello world'''
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
