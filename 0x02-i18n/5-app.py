4ebc9bf27d9f8cbb41ddf7cf62f58d8fc8d3c45d
Meacha1 <meachattd@gmail.com>
2023-08-01 22:10:28 +0300

    before_request

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -49,7 +49,7 @@ def get_locale():
 @app.route('/')
 def hello_world():
     '''Hello world'''
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
